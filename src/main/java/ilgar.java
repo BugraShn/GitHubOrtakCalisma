83dce859711ca2fff5ad5ca69d7a495654b6de85
Ilgar <ilgar.ganbarov@gmail.com>
2024-06-23 11:50:46 +0200

    ilgar gonderiyor

--- src/main/java/ilgar.java
+++ src/main/java/ilgar.java
@@ -1,5 +1,13 @@
 public class ilgar {
     public static void main(String[] args) {
+
+
+
         System.out.println("ilgar burda");
+
+
+        System.out.println("ilgar bir sheyler olusturdu");
+
+
     }
 }
