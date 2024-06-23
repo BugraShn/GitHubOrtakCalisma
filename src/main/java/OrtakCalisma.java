905e22eb786cdaaae1d04a1373a308d7593f1a9c
seren <seren@gmail.com>
2024-06-23 11:58:11 +0200

    seren2

--- src/main/java/OrtakCalisma.java
+++ src/main/java/OrtakCalisma.java
@@ -4,5 +4,7 @@ public class OrtakCalisma {
 
         System.out.println("Bugra 1.version");
         System.out.println("Kerim 2.version");
+
+        System.out.println("seren 2. version");
     }
 }
